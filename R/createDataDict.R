51f6e2d2f879d27f3980b1f60f9de9700005f595
heba-razzak <heba.razzak@hotmail.com>
2024-03-26 12:44:10 -0700

    testing

--- R/createDataDict.R
+++ R/createDataDict.R
@@ -164,7 +164,7 @@ print_data_dict <- function(data, data_title = "", descriptions = NULL) {
 
   # Print Table name & Number of Rows
   knitr::kable(df,
-               format = "markdown",)
+               format = "markdown")
 
   # Print Data Dictionary
   knitr::kable(dict_table,
